21cffdd1cad013046ec0bd31110a2fd7f0a6fddd
Xinchen Liu <mike000708@me.com>
2024-02-20 15:16:01 -0500

    Fix timeout issue in sign tests

--- tests/sign.test.ts
+++ tests/sign.test.ts
@@ -6,6 +6,7 @@ describe("Signing", () => {
     const signature = await client.sign({
       userId: "test:elonmusk",
       hash: "0xec3608877ecbf8084c29896b7eab2a368b2b3c8d003288584d145613dfa4706c",
+      auth: "",
     });
     expect(signature).toEqual({
       hash: "0xec3608877ecbf8084c29896b7eab2a368b2b3c8d003288584d145613dfa4706c",
@@ -13,11 +14,12 @@ describe("Signing", () => {
         "0xe23e1c9df7a8155381e3d21beebe7c566a6ebbe223bcc3e14c8a6dfbfd0b145463ce3b5d1cdee644a81fd1a3cf0588cb915c418d51f6a649ea9cbc7cb6d4c0ea1b",
       type: "hash",
     });
-  });
+  }, 30000);
   test("Sign string", async () => {
     const signature = await client.sign({
       userId: "test:elonmusk",
       string: "Hello World!",
+      auth: "",
     });
     expect(signature).toEqual({
       hash: "0xec3608877ecbf8084c29896b7eab2a368b2b3c8d003288584d145613dfa4706c",
@@ -25,7 +27,7 @@ describe("Signing", () => {
         "0xe23e1c9df7a8155381e3d21beebe7c566a6ebbe223bcc3e14c8a6dfbfd0b145463ce3b5d1cdee644a81fd1a3cf0588cb915c418d51f6a649ea9cbc7cb6d4c0ea1b",
       type: "string",
     });
-  });
+  }, 30000);
   test("Sign typed data", async () => {
     const signature = await client.sign({
       userId: "test:elonmusk",
@@ -59,6 +61,7 @@ describe("Signing", () => {
           contents: "Hello, Bob!",
         },
       },
+      auth: "",
     });
     expect(signature).toEqual({
       hash: "0x53e341aefc7641107dbb3d5af2a67709d7132f5cc532d728b2b610bf1e4ba72e",
@@ -66,5 +69,5 @@ describe("Signing", () => {
         "0x48732939ebce07dab77c5ef1b611bc261cd4486e894c5543648d1867a01eebaf50ddadfc3a3011a66b496ab2ed90619eca79fb3a84ffa136d016ddf3763e4f601b",
       type: "typedData",
     });
-  });
+  }, 30000);
 });
--- tests/utils/client.ts
+++ tests/utils/client.ts
@@ -2,8 +2,8 @@ import Client from "../../src";
 import { OAuthCredentials } from "../../src/types";
 
 const credentials: OAuthCredentials = {
-  clientId: Bun.env.CLIENT_ID,
-  clientSecret: Bun.env.CLIENT_SECRET,
+  clientId: Bun.env.CLIENT_ID!,
+  clientSecret: Bun.env.CLIENT_SECRET!,
 };
 
 const client = new Client(credentials);
